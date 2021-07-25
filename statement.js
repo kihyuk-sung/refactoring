bf16194ce874b4bb6a13bd4961827fe14f8a7cda
K <kihyuk.sung@outlook.kr>
2021-07-25 20:21:39 +0900

    refactor: inner function 으로 수정
    - 책의 예제와 동일하도록 수정

--- statement.js
+++ statement.js
@@ -11,44 +11,44 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf, plays).type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `  ${playFor(perf, plays).name}: ${format(amountFor(perf, plays) / 100)} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf, plays);
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-}
 
-function amountFor(aPerformance, plays) {
-  let result = 0;
-
-  switch (playFor(aPerformance, plays).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance, plays).type}`);
+  function amountFor(aPerformance) {
+    let result = 0;
+  
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+  
+    return result;
   }
 
-  return result;
-}
-
-function playFor(aPerformance, plays) {
-  return plays[aPerformance.playID];
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 module.exports = statement;
