30ed7c72f5a3a7b4757b0c89dc7f1b8f0921a445
K <kihyuk.sung@outlook.kr>
2021-07-31 20:28:48 +0900

    feat: htmlStatement

--- statement.test.js
+++ statement.test.js
@@ -1,4 +1,4 @@
-const statement = require('./statement.js');
+const { statement, htmlStatement } = require('./statement.js');
 const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
@@ -11,4 +11,20 @@ test('statement test', () => {
 총액: $1,730.00
 적립 포인트: 47점
 `);
+});
+
+
+test('htmlStatement test', ()=> {
+  expect(htmlStatement(invoice, plays)).toBe(
+`<h1>청구 내역 (고객명: BigCo)</h1>
+<table>
+<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+  <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>
+</table>
+<p>총액: <em>$1,730.00</em</p>
+<p>적립 포인트: <em>47</em>점</p>
+`
+  );
 });
